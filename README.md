9607fbb52960872cdaa18a4c41429ff21b5863fa
Jake Kohl <jacob.jp.kohl@gmail.com>
2023-11-26 00:09:07 -0600

    - Built out initial app - Created HTML frontend - Created basic logging - Implemented Doppler to avoid writing secrets in plaintext in src

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Link-Shortener-website
 
 This project is a learning solution from [DevProjects](https://www.codementor.io/projects/web/link-shortener-website-brqjanf6zq).
+
+## How to get started
+
+Run the following commands
+
+`git clone https://github.com/jakekohl/Link-Shortener-website.git`
+
+`cd Link-Shortener-Website`
+
+`npm install`
+
+Install Doppler, then get setup using
+`doppler setup`.
+
+Run command: `doppler run node app.js`
--- app.js
+++ app.js
@@ -1,19 +1,60 @@
 // init
+import express from 'express';
+import path from 'path';
+import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+const secret = process.env["BIT_KEY"]
 
 // shortener function vit bit.ly
-function shortenUrl(longUrl) {
-    fetch('https://api-ssl.bitly.com/v4/shorten', {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer aab8243332640ab3c33b83cd9c64bf0f4a2a4132',
-            'content-type': 'application/json',
-            'accept': 'application/json'
-        },
-        body: JSON.stringify({
-            url: longUrl
-        })
-    })
-    .then((response) => response.json())
-    let shortUrl = response.link
-    return shortUrl
+async function shortenUrl(longUrl) {
+    try {
+        const response = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${secret}`,
+                'content-type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify({
+                long_url: longUrl
+            })
+        });
+
+        const data = await response.json();
+        console.log(data)
+        return data.link;
+    }
+    catch(error) {
+      console.error('Error shortening URL:', error);
+      return null;
+    }
 }
+
+
+// Handle requests to the root URL
+app.get('/', (req, res) => {
+    console.log('Page loading.')
+    res.sendFile(path.join(__dirname, 'site.html'));
+    console.log('Page loaded!')
+});
+
+// Listens for async url requests
+app.get('/shorten-url/:longUrl', async (req, res) => {
+    const longUrl = req.params.longUrl;
+    console.log(`Received request for shortening the URL ${longUrl}`)
+    // Call your shortenUrl function here
+    const shortUrl = await shortenUrl(longUrl);
+    res.json({ shortUrl });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
