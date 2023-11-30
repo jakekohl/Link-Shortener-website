a8fc71341f74159cd94ae13cdd068155911bc43b
Jake Kohl <jacob.jp.kohl@gmail.com>
2023-11-29 23:22:53 -0600

    - implemented logging - finished rate-limit API output

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
+import os from 'os';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
-const secret = process.env["BIT_KEY"]
+const secret = process.env["BIT_KEY"];
+const org = process.env["BIT_ORG"];
+
+// Logging handler for server logs
+function logMessage(level, message) {
+    const loggerMessage = `${new Date().toISOString()} | ${level} | ${message}`;
+    console.log(loggerMessage);
+}
 
 // shortener function vit bit.ly
 async function shortenUrl(longUrl) {
@@ -30,7 +37,7 @@ async function shortenUrl(longUrl) {
         });
 
         const data = await response.json();
-        console.log(data);
+        logMessage('DEBUG',data);
         writeCache(data);
         return data.link;
     }
@@ -50,15 +57,15 @@ function checkCache(requestedURL) {
         if (!fs.existsSync(filePath)) {
             // If not, create it with an empty array
             fs.writeFileSync(filePath, JSON.stringify([]));
-            console.log('urlCache.json created, it was missing.')
+            logMessage('INFO','urlCache.json created, it was missing.')
         }
 
         const data = fs.readFileSync(filePath, 'utf8');
         const urlList = JSON.parse(data);
 
-        const cachedUrl = urlList.find(url => url.long_url === requestedURL);
+        const cachedUrl = urlList.find(url => url.long_url.includes(requestedURL));
         if (cachedUrl) {
-            console.log('URL found in cache:', cachedUrl.link);
+            logMessage('INFO',`URL found in cache:, ${cachedUrl.link}`);
             return cachedUrl.link;
         }
 
@@ -78,16 +85,16 @@ function writeCache(response) {
         urlList.push(response);
 
         fs.writeFileSync(filePath, JSON.stringify(urlList));
-        console.log(`urlCache.json updated with ${response.long_url} | ${response.link}.`);
+        logMessage('DEBUG',`urlCache.json updated with ${response.long_url} | ${response.link}.`);
     } catch (error) {
-        console.error('Error writing to cache:', error);
+        logMessage('ERROR',`Error writing to cache:, ${error}`);
     }
 }
 
 // Allows for manual checking of the Rate Limit for Bitly API
 async function checkLimits() {
     try {
-        const response = await fetch('https://api-ssl.bitly.com/v4/user/platform_limits', {
+        const response = await fetch(`https://api-ssl.bitly.com/v4/organizations/${org}/plan_limits`, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${secret}`,
@@ -95,44 +102,47 @@ async function checkLimits() {
                 'accept': 'application/json'
             }
         });
-        const data = await response.json();
-        console.debug(data);
 
-        const shortenEndpoint = data.find(endpoint => endpoint === '/shorten');
-        return data.methods;
+        const data = await response.json();
+        logMessage('DEBUG',data);
+        const encodes = data.plan_limits.find(item => item.name === 'encodes');
+        const count = encodes.count;
+        const limit = encodes.limit;
+        const countLimit = `${count} Used / ${limit} Total`;
+        return countLimit;
     } catch (error) {
-        console.error('Error retrieving rates:', error);
+        logMessage('ERROR',`Error retrieving rates: ${error}`);
         return null;
     }
-
 }
-
 
 // Handle requests to the root URL
 app.get('/', (req, res) => {
+    const requestor = req.ip;
+    logMessage('DEBUG',`Request from ${requestor} for root URL`);
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log('Page loaded!')
+    logMessage('INFO','Page loaded!')
 });
 
 // Listens for async shorten-url requests
 app.get('/shorten-url/:longUrl', async (req, res) => {
     const longUrl = req.params.longUrl;
-    console.log(`Received request for shortening the URL ${longUrl}`)
+    logMessage('DEBUG',`Received request for shortening the URL ${longUrl}`)
     let cacheCheck = checkCache(longUrl);
     if (cacheCheck) {
-        console.log('Returning:', cacheCheck);
+        logMessage('DEBUG',`Returning:, ${cacheCheck}`);
         return cacheCheck;
     }
     // Call your shortenUrl function here
     const shortUrl = await shortenUrl(longUrl);
-    console.log('Returning:', shortUrl);
+    logMessage('DEBUG',`Returning:, ${shortUrl}`);
     res.json({ shortUrl });
 });
 
 // Listens for async check-limits requests
 app.get('/check-limits', async (req, res) => {
     const limits = await checkLimits();
-    console.log('Returning:', limits);
+    logMessage('DEBUG',`Returning:, ${limits}`);
     res.json({ limits });
 });
 
